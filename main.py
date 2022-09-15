1bdac858fab2bef858bce206413eb530a4ea97a4
T-otor <victor.uettwiller@uha.fr>
2022-09-15 19:18:27 +0200

    Connexion fini + récupération de tous les hosts monitoré

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from pyzabbix import ZabbixAPI
 
 
 # Zabbix API connection
-zapi = ZabbixAPI("http://mon.ecowan.network")
-zapi.login("zabbix user", "zabbix pass")
+zapi = ZabbixAPI("https://mon.ecowan.network")
+zapi.login("api-minitel", "connard68")
 print("Connecté au serveur Zabbix d'ECOWAN. Version %s" % zapi.api_version())
+
+# Get all hosts
+hosts = zapi.host.get(output="extend")
+for h in hosts:
+    print("Host: %s" % h["host"])
+
