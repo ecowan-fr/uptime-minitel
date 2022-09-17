d3c42c5eaf12f944247a96668162a4012b522063
T-otor <victor.uettwiller@uha.fr>
2022-09-17 19:41:37 +0200

    Affichage du logo (test calibration)

--- screen.py
+++ screen.py
@@ -1,24 +1,22 @@
-<<<<<<< HEAD
 #Programme permettant de lancer un programme en plein écran sur le minitel
 
 import os
-=======
->>>>>>> d00fab818950fed315e3aa5c4ac9665b66e9db2f
 import sys
 import time
 import random
 import logging
-<<<<<<< HEAD
 from pyzabbix import ZabbixAPI
 import serial
 import pynitel
 
 
 minitel = serial.Serial("COM3", 1200, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_TWO, timeout=1, bytesize=7)
-minitel.write(b'\n') #Saute une ligne
 minitel.write(b'\r') #Reviens au début de la ligne
+minitel.write(b'\x1b[2J') #Efface l'écran
+logo = open("ecowan.txt", "tr")
+lines = logo.readlines()
+for line in lines:
+    minitel.write(line.encode())
+    minitel.write(b'\r')
+    time.sleep(0.1)
 
-=======
-
-#Initialisation liaison série USB --> Minitel
->>>>>>> d00fab818950fed315e3aa5c4ac9665b66e9db2f
